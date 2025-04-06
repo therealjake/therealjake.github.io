4c7039b5f0ccb1baf91377638658ac56bc470fc8
Jake <jake_battle@yahoo.com>
2025-04-06 16:47:53 -0500

    First shadowbear build

---
+++ static/js/198.b283ddb6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkshadowbear=self.webpackChunkshadowbear||[]).push([[198],{1198:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=198.b283ddb6.chunk.js.map
